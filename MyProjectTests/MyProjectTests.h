ee8b3e05cf096c546f30377dc1eab25c3b0f0158
Matthew <Matthew@TJs-Mac-Pro.local>
2012-07-31 17:12:30 -0700

    Initial Commit

---
+++ MyProjectTests/MyProjectTests.h
@@ -0,0 +1,13 @@
+//
+//  MyProjectTests.h
+//  MyProjectTests
+//
+//  Created by Matthew on 7/31/12.
+//  Copyright (c) 2012 Matthew. All rights reserved.
+//
+
+#import <SenTestingKit/SenTestingKit.h>
+
+@interface MyProjectTests : SenTestCase
+
+@end
---
+++ MyProjectTests/MyProjectTests.m
@@ -0,0 +1,32 @@
+//
+//  MyProjectTests.m
+//  MyProjectTests
+//
+//  Created by Matthew on 7/31/12.
+//  Copyright (c) 2012 Matthew. All rights reserved.
+//
+
+#import "MyProjectTests.h"
+
+@implementation MyProjectTests
+
+- (void)setUp
+{
+    [super setUp];
+    
+    // Set-up code here.
+}
+
+- (void)tearDown
+{
+    // Tear-down code here.
+    
+    [super tearDown];
+}
+
+- (void)testExample
+{
+    STFail(@"Unit tests are not implemented yet in MyProjectTests");
+}
+
+@end
